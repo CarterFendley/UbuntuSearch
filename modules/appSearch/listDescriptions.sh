a5828a303c51541d4b029161f89af33c76cfa838
carter.fendley <carter.fendley@gmail.com>
2016-02-18 00:14:41 -0500

    Added psuedo code

--- modules/appSearch/listDescriptions.sh
+++ modules/appSearch/listDescriptions.sh
@@ -5,7 +5,18 @@ for app in $(dpkg-query --show | awk '{print $1}')
 do
     printf "\b${sp:i++%${#sp}:1}"
     while read description; do
-        printf "$app:\n$description\n\n" >> modules/appSearch/installedPackagesDescriptions.list
+        service=$(service --status-all | grep $app)
+        if [ "$service" = "" ]; then
+            service="\nService: non-existant"
+        else
+            if [ "$(status $app)" = "status: Unknown job: $app" ]; then
+                service="\nService: non-existant"
+            else
+                status=$(status $app | tr -d \'$app\')
+                service="\nService:$status"
+            fi
+        fi
+        printf "$app:$service\n$description\n\n" >> modules/appSearch/installedPackagesDescriptions.list
     done < <(apt-cache show $app | grep Description-en:)
 done
 printf "\n"
